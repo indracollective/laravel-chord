1324a6c08c046cea45006b3e7ea7eed2d7526f5f
Shea <shea@livesource.co.nz>
2024-08-16 15:36:54 +1200

    progress

--- src/Filament/Resources/PageResource.php
+++ src/Filament/Resources/PageResource.php
@@ -6,11 +6,13 @@ use CodeWithDennis\FilamentSelectTree\SelectTree;
 use Filament\Forms\Components\Builder;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Support\Str;
 use LiveSource\Chord\Facades\Chord;
 use LiveSource\Chord\Models\Page;
@@ -21,44 +23,38 @@ class PageResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    public static function formTabs(Form $form)
+    {
+        return Tabs::make('Tabs')
+            ->tabs([
+                $this->generalFormTab($form),
+                $this->seoFormTab($form),
+            ]);
+    }
+
+    public static function generalFormTab(Form $form): Tabs
+    {
+        return Tab::make('General')->schema([
+
+        ])
+    }
+
     public static function form(Form $form): Form
     {
         $blockTypes = collect(Chord::getBlockTypes())->map(function ($type) {
             return $type::getBuilderBlock();
         })->toArray();
 
-        return $form
-            ->schema([
-                Grid::make(3)
+        return $form->schema([ static::formTabs($form) ]);
+            TextInput::make('title')->required(),
+            TextInput::make('slug')->required(),
+                SelectTree::make('parent_id')
+                    ->relationship('parent', 'title', 'parent_id'),
+                Section::make('blocks-section')
                     ->schema([
-                        Section::make('Form')
-                            ->columnSpan(1)
-                            ->schema([
-                                TextInput::make('title')
-                                    ->afterStateUpdated(function ($get, $set, ?string $state) {
-                                        if (! $get('meta.is_slug_changed_manually') && filled($state)) {
-                                            $set('slug', Str::slug($state));
-                                        }
-                                    })
-                                    ->reactive()
-                                    ->live(onBlur: false, debounce: 500)
-                                    ->required(),
-                                TextInput::make('slug')
-                                    ->afterStateUpdated(function ($set) {
-                                        $set('meta.is_slug_changed_manually', true);
-                                    })
-                                    ->reactive()
-                                    ->required(),
-                                SelectTree::make('parent_id')
-                                    ->relationship('parent', 'title', 'parent_id'),
-                                Section::make('blocks-section')
-                                    ->schema([
-                                        Builder::class::make('blocks')
-                                            ->blocks($blockTypes),
-                                    ]),
-                            ]),
+                        Builder::class::make('blocks')
+                            ->blocks($blockTypes),
                     ]),
-
             ]);
     }
 
@@ -70,7 +66,7 @@ class PageResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('title')
                     ->getStateUsing(function (Page $record) {
-                        return self::getNestedPrefix($record->id) . ($record->parent_id ? '--&nbsp;' : '') . $record->title;
+                        return self::getNestedPrefix($record->id).($record->parent_id ? '--&nbsp;' : '').$record->title;
                     })
                     ->html(),
                 Tables\Columns\TextColumn::make('slug'),
@@ -79,36 +75,23 @@ class PageResource extends Resource
                 Tables\Columns\TextColumn::make('order_column'),
 
             ])
-            ->filters([
-                //
-            ])
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\Action::make('visual')
                     ->label('Visual edit')
-                    ->url(fn (Page $record): string => route('filament.admin.resources.pages.visual', $record)),
+                    ->url(fn(Page $record): string => route('filament.admin.resources.pages.visual', $record)),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
-            ]);
-    }
-
-    private static function getNestedPrefix($parent_id, $prefix = ''): string
-    {
-        static $parents = null;
-        if ($parents == null) {
-            $parents = self::$model::all()->pluck('parent_id', 'id');
-        }
-        if ($parent_id == 0) {
-            return '';
-        }
-        if (isset($parents[$parent_id]) && $parents[$parent_id]) {
-            $prefix .= self::getNestedPrefix($parents[$parent_id], $prefix . '&nbsp;&nbsp;');
-        }
-
-        return $prefix;
+            ])
+            ->modifyQueryUsing(function (EloquentBuilder $query): EloquentBuilder {
+                if (request()->has('parent')) {
+                    return $query->where('parent_id', request()->get('parent'));
+                }
+                return $query->where('parent_id', null);
+            });
     }
 
     public static function getRelations(): array
--- src/Filament/Resources/PageResource/Pages/ListPages.php
+++ src/Filament/Resources/PageResource/Pages/ListPages.php
@@ -15,6 +15,8 @@ class ListPages extends ListRecords
 
     protected ?string $maxContentWidth = 'full';
 
+    protected ?Page $parent;
+
     protected function getHeaderActions(): array
     {
         return [
@@ -24,49 +26,6 @@ class ListPages extends ListRecords
 
     public function reorderTable(array $order): void
     {
-
         Page::setNewOrder($order);
-    }
-
-    protected function getTableQuery(): Builder
-    {
-        $orderedIds = static::getNestedArray();
-
-        // if database is sqlite...
-
-        if (DB::connection()->getDriverName() == 'sqlite') {
-            return static::getResource()::getEloquentQuery()->orderByRaw(
-                'CASE id ' .
-                implode(' ', array_map(
-                    fn ($id, $index) => "WHEN $id THEN " . ($index + 1),
-                    $orderedIds,
-                    array_keys($orderedIds)
-                )) . ' ELSE 9999 END'
-            );
-        } elseif (DB::connection()->getDriverName() == 'mysql') {
-            return static::getResource()::getEloquentQuery()->orderByRaw(sprintf('parent_id(id, %s)', implode(',', $orderedIds)));
-        } else {
-            throw new \Exception('Unsupported database driver');
-        }
-    }
-
-    public static function getNestedArray($parent_id = null): array
-    {
-        static $records = null;
-        static $ids = [];
-        if ($records == null) {
-            $records = self::getResource()::getModel()::get(['id', 'parent_id'])->sortByDesc('id')->groupBy('parent_id')->sortBy('parent_id')->toArray();
-        }
-
-        if (isset($records[$parent_id]) && count($records[$parent_id])) {
-            foreach ($records[$parent_id] as $_id => $_item) {
-                $ids[] = $_item['id'];
-                if (isset($records[$_item['id']]) && is_array($records[$_item['id']]) && count($records[$_item['id']])) {
-                    self::getNestedArray($_item['id']);
-                }
-            }
-        }
-
-        return $ids;
     }
 }
